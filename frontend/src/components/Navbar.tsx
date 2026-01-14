30abb559cafd04eda3999fec8d9946e3dd09ed18
toriisimone <toriisimone@gmail.com>
2026-01-14 01:39:33 -0500

    header updated

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -86,11 +86,12 @@ const Navbar = () => {
 
   return (
     <>
-      {/* Main Header - Kylie's Exact Structure */}
+      {/* Main Header */}
       <header className={styles.siteHeader}>
         <div className={styles.siteHeaderWrapper}>
-          {/* Announcement Bar */}
+          {/* Top Banner - Logo, Shipping, Icons */}
           <div className={styles.siteHeaderAnnouncement}>
+            {/* Shipping to US - Left */}
             <form className={styles.localizationForm} method="post" action="/localization" id="localization_form" acceptCharset="UTF-8" encType="multipart/form-data">
               <input type="hidden" name="form_type" value="localization" />
               <input type="hidden" name="utf8" value="✓" />
@@ -142,23 +143,102 @@ const Navbar = () => {
                 </select>
               </div>
             </form>
-          </div>
 
-          {/* Main Header Row */}
-          <div className={styles.siteHeaderMain}>
-            {/* Logo */}
+            {/* Logo - Center */}
             <div className={styles.siteHeaderLogo}>
               <h1 className={styles.siteLogo}>
                 <Link to="/" rel="follow" title="AURAPOP home page">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 194 51" role="img" aria-labelledby="AURAPOP Logo">
-                    <title id="svgTitle">AURAPOP Homepage</title>
-                    <path fill="currentColor" d="M50.427 35.174v4.015l3.23-4.015h1.316l-3.336 4.13 3.524 5.02h-1.496l-3.247-4.775v4.775H49.25v-9.15h1.178ZM58.718 35.174l1.8 4.424 1.84-4.424h1.193l-2.51 5.479v3.671h-1.153V40.62l-2.47-5.454h1.308l-.008.008ZM67.893 35.174v8.136h3.344v1.014h-4.513v-9.15h1.169ZM75.71 35.174v9.15h-1.177v-9.15h1.178ZM84.657 35.174v1.014h-3.55v2.944h3.296v1.006h-3.295v3.18h3.639v1.014h-4.808v-9.15h4.718v-.008ZM97.332 35.174v6.812c0 1.61-.81 2.518-2.47 2.518-1.512 0-2.346-.826-2.346-2.6 0-.188 0-.303.008-.442h1.218c-.008.09-.008.213-.008.352 0 .769.074 1.7 1.145 1.7.875 0 1.292-.645 1.292-1.733v-6.607h1.17-.009ZM106.253 35.174v1.014h-3.548v2.944H106v1.006h-3.295v3.18h3.638v1.014h-4.808v-9.15h4.718v-.008ZM111.609 35.174l3.598 7.834v-7.834h1.039v9.15h-1.586l-3.639-7.932v7.932h-1.055v-9.15h1.643ZM122.101 35.174l3.598 7.834v-7.834h1.038v9.15h-1.586l-3.639-7.932v7.932h-1.055v-9.15h1.644ZM135.61 35.174v1.014h-3.549v2.944h3.295v1.006h-3.295v3.18h3.639v1.014h-4.808v-9.15h4.718v-.008ZM142.479 35.174c1.194 0 2.142.785 2.142 2.273 0 1.129-.605 1.914-1.676 2.118.989.164 1.472.622 1.496 2.355.017.85.115 1.62.303 2.404h-1.219a3.2 3.2 0 0 1-.212-1.038c-.025-.597-.09-1.48-.098-1.71-.09-1.332-.687-1.455-1.791-1.455h-.99v4.211h-1.177v-9.15h3.222v-.008Zm-2.045 1.014v2.944h1.423c1.088 0 1.57-.556 1.57-1.538 0-.981-.482-1.406-1.316-1.406h-1.677ZM14.275 6.807h4.17l-5.078 8.144 5.078 10.01h-4.277l-4.35-9.307h-.05v9.306H5.976V6.807h3.794v7.768h.049l4.457-7.768Zm8.823 18.153h3.794v-6.288L31.97 6.807h-3.876l-2.895 7.67-2.706-7.67h-4.146l4.75 11.865v6.288Zm20.304 0v-3.017H37.72V6.807h-3.794V24.96h9.477Zm1.832 0h3.794V6.807h-3.794V24.96Zm15.733 0v-2.788h-6.313v-5.307h5.634v-2.789h-5.634v-4.48h6.109v-2.79h-9.911V24.96H60.967Zm54.616 0V10.952h.049l3.32 14h3.623l3.492-14h.049v14h3.696V6.807h-5.904l-3.018 12.34h-.049l-2.837-12.34h-6.109V24.96h3.697-.009Zm26.168 0v-2.788h-6.313v-5.307h5.634v-2.789h-5.634v-4.48h6.108v-2.79h-9.902V24.96h10.107Zm1.643-18.153v3.017h4.228V24.96h3.794V9.824h4.228V6.807h-12.25Zm17.868 18.153V6.807h-3.794V24.96h3.794Zm-84.112.295c3.54 0 5.479-1.562 5.479-6.411h-3.745c-.05 1.43.024 3.696-1.734 3.696-2.134 0-2.412-2.012-2.412-6.665 0-4.653.278-6.664 2.412-6.664 1.128 0 1.562.907 1.562 3.32h3.72c.148-3.893-1.406-6.035-5.282-6.035-6.264 0-6.264 4.571-6.264 9.38 0 4.807 0 9.379 6.264 9.379Zm13.435 0c6.256 0 6.256-4.629 6.256-9.38 0-4.75 0-9.38-6.256-9.38-6.255 0-6.263 4.572-6.263 9.38 0 4.808 0 9.38 6.263 9.38Zm0-2.715c-2.134 0-2.412-2.012-2.412-6.665 0-4.653.278-6.664 2.412-6.664 2.135 0 2.413 2.011 2.413 6.664s-.278 6.665-2.413 6.665Zm19.65-2.535c0-5.708-7.645-5.405-7.645-8.848 0-1.26.727-1.938 1.962-1.938 1.529 0 1.758 1.382 1.758 2.74h3.696c.254-3.795-1.586-5.455-5.282-5.455-4.628 0-5.986 2.265-5.986 5.103 0 5.405 7.646 5.61 7.646 8.872 0 1.235-.63 2.06-1.914 2.06-2.085 0-2.158-1.455-2.158-3.197h-3.795c-.204 3.14.679 5.913 5.381 5.913 2.887 0 6.337-.556 6.337-5.25Zm59.122 5.25c3.541 0 5.479-1.562 5.479-6.411h-3.745c-.049 1.43.025 3.696-1.734 3.696-2.134 0-2.412-2.012-2.412-6.665 0-4.653.278-6.664 2.412-6.664 1.129 0 1.562.907 1.562 3.32h3.721c.147-3.893-1.407-6.035-5.283-6.035-6.263 0-6.263 4.571-6.263 9.38 0 4.807 0 9.379 6.263 9.379Zm18.669-5.25c0-5.708-7.646-5.405-7.646-8.848 0-1.26.728-1.938 1.963-1.938 1.529 0 1.758 1.382 1.758 2.74h3.696c.254-3.795-1.586-5.455-5.282-5.455-4.629 0-5.986 2.265-5.986 5.103 0 5.405 7.646 5.61 7.646 8.872 0 1.235-.63 2.06-1.914 2.06-2.085 0-2.159-1.455-2.159-3.197h-3.794c-.204 3.14.679 5.913 5.381 5.913 2.886 0 6.337-.556 6.337-5.25Z"></path>
-                  </svg>
+                  AURAPOP
                 </Link>
               </h1>
             </div>
 
-            {/* Navigation */}
+            {/* Desktop Utilities - Icons Right */}
+            <div className={`${styles.siteHeaderUtilities} ${styles.siteHeaderUtilitiesDesktop}`}>
+              {/* Search */}
+              <Link
+                to="/search"
+                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionSearch}`}
+                title="Search"
+                rel="nofollow"
+                role="button"
+              >
+                <svg width="20" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="11" cy="11" r="7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                  <path d="M17 17l4 4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                </svg>
+              </Link>
+              {/* Account */}
+              <Link
+                to="/account"
+                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionAccount}`}
+                aria-label="Account Login"
+                title="Account"
+                rel="nofollow"
+              >
+                <svg width="20" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="12" cy="8" r="4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                  <path d="M6 20c0-3.33 2.67-6 6-6s6 2.67 6 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                </svg>
+              </Link>
+              {/* Wishlist */}
+              <Link
+                to="/wishlist"
+                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionWishlist}`}
+                aria-label="Product Added in Wishlist"
+                title="Wishlist"
+                rel="nofollow"
+              >
+                <svg width="21" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                </svg>
+              </Link>
+              {/* Cart */}
+              <Link
+                to="/cart"
+                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionCart}`}
+                title="cart"
+                rel="nofollow"
+                role="button"
+                data-cart-count={getItemCount()}
+                aria-label={`${getItemCount()} items in cart`}
+              >
+                <svg width="19" height="23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6 9L4 20H20L18 9H6Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                  <path d="M4 9H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M6 9C6 8 6.5 7 7.5 7C8.5 7 9 8 9 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                  <path d="M15 9C15 8 15.5 7 16.5 7C17.5 7 18 8 18 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                </svg>
+                {getItemCount() > 0 && (
+                  <span className={styles.cartBadge}>{getItemCount()}</span>
+                )}
+                <span className={styles.visuallyHidden}>Cart with {getItemCount()} items</span>
+              </Link>
+            </div>
+
+            {/* Mobile Menu Toggle */}
+            <div className={`${styles.siteHeaderUtilities} ${styles.siteHeaderUtilitiesMenu}`}>
+              <Link
+                to="/collections"
+                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionMenu}`}
+                title="Main Menu"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded={mobileMenuOpen}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setMobileMenuOpen(!mobileMenuOpen);
+                }}
+              >
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M3 12h18M3 6h18M3 18h18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+                </svg>
+              </Link>
+            </div>
+          </div>
+
+          {/* Second Row - Navigation Menu */}
+          <div className={styles.siteHeaderMain}>
             <nav className={styles.siteHeaderNav}>
               <div className={styles.siteHeaderMenuDesktop}>
                 <div id="shopify-section-header-menu--desktop" className={styles.shopifySection}>
@@ -424,89 +504,6 @@ const Navbar = () => {
                 </div>
               </div>
             </nav>
-
-            {/* Desktop Utilities - All Icons */}
-            <div className={`${styles.siteHeaderUtilities} ${styles.siteHeaderUtilitiesDesktop}`}>
-              {/* Search */}
-              <Link
-                to="/search"
-                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionSearch}`}
-                title="Search"
-                rel="nofollow"
-                role="button"
-              >
-                <svg width="20" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="11" cy="11" r="7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                  <path d="M17 17l4 4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                </svg>
-              </Link>
-              {/* Account */}
-              <Link
-                to="/account"
-                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionAccount}`}
-                aria-label="Account Login"
-                title="Account"
-                rel="nofollow"
-              >
-                <svg width="20" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="12" cy="8" r="4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                  <path d="M6 20c0-3.33 2.67-6 6-6s6 2.67 6 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                </svg>
-              </Link>
-              {/* Wishlist */}
-              <Link
-                to="/wishlist"
-                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionWishlist}`}
-                aria-label="Product Added in Wishlist"
-                title="Wishlist"
-                rel="nofollow"
-              >
-                <svg width="21" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                </svg>
-              </Link>
-              {/* Cart */}
-              <Link
-                to="/cart"
-                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionCart}`}
-                title="cart"
-                rel="nofollow"
-                role="button"
-                data-cart-count={getItemCount()}
-                aria-label={`${getItemCount()} items in cart`}
-              >
-                <svg width="19" height="23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M6 9L4 20H20L18 9H6Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                  <path d="M4 9H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  <path d="M6 9C6 8 6.5 7 7.5 7C8.5 7 9 8 9 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                  <path d="M15 9C15 8 15.5 7 16.5 7C17.5 7 18 8 18 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-                </svg>
-                {getItemCount() > 0 && (
-                  <span className={styles.cartBadge}>{getItemCount()}</span>
-                )}
-                <span className={styles.visuallyHidden}>Cart with {getItemCount()} items</span>
-              </Link>
-            </div>
-
-            {/* Mobile Menu Toggle */}
-            <div className={`${styles.siteHeaderUtilities} ${styles.siteHeaderUtilitiesMenu}`}>
-              <Link
-                to="/collections"
-                className={`${styles.siteHeaderAction} ${styles.siteHeaderActionMenu}`}
-                title="Main Menu"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded={mobileMenuOpen}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setMobileMenuOpen(!mobileMenuOpen);
-                }}
-              >
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3 12h18M3 6h18M3 18h18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-                </svg>
-              </Link>
-            </div>
           </div>
         </div>
 
